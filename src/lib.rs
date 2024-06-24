c3bc655f12f504ae3e86e625615f63ae34c6613e
Abhinav Pandey <abhinav.pandey.1512@gmail.com>
2024-06-25 01:32:22 +0530

    feat: add commmon utils

--- src/lib.rs
+++ src/lib.rs
@@ -8,3 +8,10 @@ pub use color::Color;
 pub use hittable::{HitRecord, Hittable};
 pub use ray::Ray;
 pub use vec3::{Point3D, Vector3};
+
+pub const INFINITY: f64 = f64::INFINITY;
+pub const PI: f64 = std::f64::consts::PI;
+
+pub fn degree_to_rad(degree: f64) -> f64 {
+    degree * PI / 180.0
+}
