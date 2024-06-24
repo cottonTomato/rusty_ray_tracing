1b66cbee588f1078880945edfff31863ee7dab99
Abhinav Pandey <abhinav.pandey.1512@gmail.com>
2024-06-25 00:54:07 +0530

    feat: add HitableList class

--- src/lib.rs
+++ src/lib.rs
@@ -4,6 +4,6 @@ pub mod ray;
 pub mod vec3;
 
 pub use color::Color;
-pub use hittable::{HitRecord, Hittable};
+pub use hittable::{HitRecord, Hittable, HittableList};
 pub use ray::Ray;
 pub use vec3::{Point3D, Vector3};
