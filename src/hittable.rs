a4ddc9433eca059e2efffc24fbb3dd265ea43945
Abhinav Pandey <abhinav.pandey.1512@gmail.com>
2024-06-25 00:51:41 +0530

    feat: add hittable trait

---
+++ src/hittable.rs
@@ -0,0 +1,33 @@
+use crate::{Point3D, Ray, Vector3};
+
+#[derive(Debug, Default, Clone, Copy)]
+pub struct HitRecord {
+    pub point: Point3D,
+    pub normal: Vector3,
+    pub t: f64,
+    pub front_face: bool,
+}
+
+impl HitRecord {
+    pub fn new(t: f64, ray: Ray, outward_normal_logic: impl Fn(Point3D) -> Vector3) -> Self {
+        let point = ray.at(t);
+        let outward_normal = outward_normal_logic(point);
+        let front_face = ray.direction().dot(outward_normal) < 0.0;
+        let normal = if front_face {
+            outward_normal
+        } else {
+            -outward_normal
+        };
+
+        HitRecord {
+            t,
+            point,
+            normal,
+            front_face,
+        }
+    }
+}
+
+pub trait Hittable {
+    fn hit(&self, ray: Ray, t_min: f64, t_max: f64) -> Option<HitRecord>;
+}
--- src/lib.rs
+++ src/lib.rs
@@ -1,7 +1,9 @@
 pub mod color;
+pub mod hittable;
 pub mod ray;
 pub mod vec3;
 
 pub use color::Color;
+pub use hittable::{HitRecord, Hittable};
 pub use ray::Ray;
 pub use vec3::{Point3D, Vector3};
